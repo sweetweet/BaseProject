a1bdbe6754b343ed5ef44d8f888f7285c8efa83e
sweetweet <sweetweetbb@gmail.com>
2017-12-08 20:34:48 +0800

    Create Triangle2

--- src/test/java/com/github/sweetweet/TriangleTest.java
+++ src/test/java/com/github/sweetweet/TriangleTest.java
@@ -18,4 +18,17 @@ public class TriangleTest {
         Assert.assertEquals("0", result);
     }
 
+    public void triangle2() throws Exception {
+        //Arrange
+        Triangle tri = new Triangle(3, 5, 3);
+
+        //Act
+        String result = tri.triangle();
+
+        //Assert
+        Assert.assertEquals("1", result);
+    }
+
 }
+
+
